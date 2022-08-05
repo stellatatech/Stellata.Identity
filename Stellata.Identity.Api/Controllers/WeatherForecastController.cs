0e4bed8d3a26812ebc59fca25cb6debfd8cd7f4b
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2022-08-05 14:06:48 -0500

    Update WeatherForecastController.cs

--- Stellata.Identity.Api/Controllers/WeatherForecastController.cs
+++ Stellata.Identity.Api/Controllers/WeatherForecastController.cs
@@ -21,11 +21,12 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        var envar = Environment.GetEnvironmentVariable("HEROKU_USERNAME");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = envar;
             })
             .ToArray();
     }
